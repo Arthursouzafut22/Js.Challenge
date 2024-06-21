38eda3e5d1805e03e2d632b59e1c8f3a130c0e52
Arthursouzafut22 <arthursouzafut22@gmail.com>
2024-06-21 14:38:39 -0300

    New challenges

--- slide.js
+++ slide.js
@@ -1042,6 +1042,180 @@ console.log(perdidaoMultidao('bEEFGBuFBRrHgUHlNFYaYr'));
 
 
 
+function doisArray(array1,array2) {
+
+  return [...array1,...array2].sort((a,b) => a - b);
+
+}
+
+console.log(doisArray([1, 10], [2, 3, 4, 5, 6, 7, 8, 9]));
+
+
+
+function quemEmaior(array1,array2) {
+
+  if(array1 > array2) return true;
+  else return false;
+
+}
+
+console.log(quemEmaior([true, 10, "zebra"], [true, 10, "zebra"]));
+
+
+
+function negacaoZero(num) {
+
+  const zero = num--;
+  const um = num++;
+
+  return zero,Math.abs(um);
+
+}
+
+
+console.log(negacaoZero(1));
+
+
+
+function divisaoNumerica(numero) {
+
+  const div1 = numero / 2;
+  const div2 = numero / 2;
+
+  if(numero % 2 !== 0) return [
+    Math.floor(div1), Math.ceil(div2)
+  ];
+  
+  return [div1,div2];
+
+}
+
+console.log(divisaoNumerica(1));
+
+
+function filterPares(array) {
+  return array.filter(n => n % 2 === 0);
+
+}
+
+console.log(filterPares([1, 3, 2, 6, 4, 8]));
+
+
+
+
+function filterString(array) {
+
+  return array.filter((item) => typeof item === 'number');
+
+}
+
+console.log(filterString([1, 2, "aasf", "1", "123", 123]));
+
+
+
+function erro(numero) {
+
+  const erros = [ "Check the fan: e1","Emergency stop: e2",
+    "Pump Error: e3","c: e4","Temperature Sensor Error: e5"];
+
+    let value = [];
+    
+    for(let err of erros) {
+      value.push(err);
+    }
+
+    return value[numero - 1];
+
+}
+
+console.log(erro(4));
+
+
+
+
+
+function repeatStrinTho(string) {
+
+  let strin = "";
+
+  for(let str of string) {
+    let repet = str.repeat(2);
+    strin += repet;
+  }
+
+  return strin;
+}
+
+
+console.log(repeatStrinTho('Hello World!'));
+
+
+
+
+function orderArray(array) {
+  let reverse = array.sort()
+
+  return reverse;
+
+}
+
+console.log(orderArray(["may", "april", "september", "august"]));
+
+
+
+
+function facamimRir(str) {
+
+  if(str === 'Make me smile') return str.replace('smile',':D'); 
+  else if(str === "Make me grin") return str.replace('grin',':)'); 
+  else if(str === 'Make me sad') return str.replace('sad',':('); 
+
+  return str;
+}
+
+console.log(facamimRir('Make me grin'));
+
+
+
+function digitoMaisAlto(numero) {
+
+  const newString = numero.toString();
+  const splitNew = newString.split('');
+
+  const loop = splitNew.map((item) => Number(item));
+
+  return Math.max(...loop);
+
+}
+
+console.log(digitoMaisAlto(77899));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
